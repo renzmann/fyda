d619cdab6962558f9fc5b1029f586a00a6810adf
Robert Enzmann <robert.enzmann@antuit.com>
2018-10-03 09:58:24 -0500

    configuration examples

---
+++ configure.py
@@ -0,0 +1,9 @@
+# Example configuration script for adding/working with data in fyda
+#
+# import fyda
+# 
+#
+# fyda.add_data('data', 'data.csv')
+# fyda.add_data('data_ex', 'datax.xlsx')
+# fyda.configure_excel('data_ex', key='sheet_name', value='Sheet1')
+#
