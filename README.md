d9241a9d2359656bea326d0d38d77b2f76255160
Robert Enzmann <robert.enzmann@antuit.com>
2018-10-15 14:20:55 -0500

    update for pypi build

--- README.md
+++ README.md
@@ -11,42 +11,14 @@ the available data in the ``input`` folder, and use fyda's built-in api to
 load it, transform it, and model it. 
 
 
+## Installation
+
+```sh
+pip install fyda
+```
+
+
 ## User guide
 
 You should [Read The Docs](https://fyda.readthedocs.io/en/latest/) for all
 the goodies!
-
-
-## Installation
-
-Currently the only way to get fyda is by installing it manually from github.
-fyda is developed with the Anaconda distribution virtual environment
-achitecture in mind but you can use whatever venv setup you like.
-Assuming your current working directory is where you want to put this repo, 
-
-```sh
-git clone https://github.com/renzmann/fyda
-```
-
-
-Next, install the requirements
-
-(Linux / macOS)
-
-```sh
-$ while read requirement; do conda install --yes $requirement; done < requirements.txt
-```
-
-(Windows)
-
-```sh
-> FOR /F "delims=~" %f in (requirements.txt) DO conda install --yes "%f" || pip install "%f"
-```
-
-Finally, install fyda
-
-```sh
-pip install .
-```
-
-
--- setup.py
+++ setup.py
@@ -1,12 +1,30 @@
 """Setup."""
 from setuptools import setup
 
-setup(name='fyda',
-      version='0.1.1',
-      description='General data interface for Python 3',
-      url='http://github.com/renzmann/ifydat',
-      author='Robert Enzmann',
-      author_email='robert.enzmann@antuit.com',
-      license='MIT',
-      packages=['fyda'],
-      zip_safe=False)
+setup(
+  name = 'fyda',
+  packages = ['fyda'],
+  version = '0.1',
+  license='MIT',
+  description = 'General data interface for Python 3',
+  author = 'Robert Enzmann',
+  author_email = 'runningwithrobb@gmail.com',
+  url = 'https://github.com/renzmann/fyda',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = ['Python', 'Data', 'Interface', 'Data Science', 'python 3'],
+  install_requires=[
+          'numpy',
+          'pandas',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
