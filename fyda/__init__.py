7d3e4dfc32044bffeebdc446cc781435dab20ea5
Robert Enzmann <robert.enzmann@antuit.com>
2019-03-28 10:22:11 -0500

    Fix new fyda __init__

--- fyda/__init__.py
+++ fyda/__init__.py
@@ -1,19 +1,3 @@
 """fyda - the interface for your data"""
-from .configurate import (
-    add_data,
-    remove_data,
-    remove_directory,
-    set_data_root,
-    remove_option,
-    add_option,
-    sections,
-    add_section,
-    remove_section,
-    get_shortcut,
-    dir_path,
-    data_path,
-)
-from .util import load_data, summary
 from .base import DataBank, ProjectConfig
-from .base import load_s3_obj
-from .base import load
+from .base import load, load_s3, data_path
