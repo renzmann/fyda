405d3947eb5623c88f605fde420b4c5569d461c7
renzmann <robert.enzmann@antuit.com>
2019-08-01 06:14:42 -0500

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md', 'r') as fh:
 setup(
     name='fyda',
     packages=['fyda'],
-    version='0.4.4',
+    version='0.4.5',
     license='MIT',
     description='General data interface for Python 3',
     long_description=long_description,
